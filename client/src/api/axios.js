422e29bcaaf850a6cdb0094f73a87bf0920255f1
Yaswanth0525 <yaswanthbogila@gmail.com>
2026-05-23 10:11:42 +0530

    for deployment changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api', // Backend URL
+    baseURL: import.meta.env.VITE_API_URL, // Backend URL
 });
 
 // Add a request interceptor to attach the JWT token
